5de6ef72271a0ee948e0d2c3cbaf5681eb9ebcd5
sandipan <sandipan.dey@gmail.com>
2014-06-28 00:00:42 +0530

    Modified

--- shinyProject/ui.R
+++ shinyProject/ui.R
@@ -3,14 +3,29 @@ shinyUI(pageWithSidebar(
   headerPanel("Shiny with mtcars"),
 
   sidebarPanel(
-    conditionalPanel(condition="input.conditionedPanels == 'Tables'",  
-           numericInput("obs", "Number of observations to view:", min = 0, max = nrow(mtcars), value = 5),
+
+	h4("Description"),
+	p("The application is meant to provide a simple visualization for the mtcars dataset.", 
+	"This Motor Trend Car Road Tests dataset comes along with the R base packages.",
+	"It comprises of the fuel consumption and 10 aspects of automobile design / performance."),
+	conditionalPanel(condition="input.conditionedPanels == 'Tables'",  
+		   p("In the first tab (Tables), first select a variable (such as fuel consumption mpg) 
+		   from the drop down: The summary statistics for the variable will be shown immediately.", 
+		   "Also, enter the number of observations for the selected variable to view, in the text box",
+		   "only that many rows of values for that variable will be shown"),
            selectInput(inputId = "var",
                        label = "Select a variable to display the summary statistics",
                        choices = names(mtcars),
-                       selected = "mpg")
+                       selected = "mpg"),
+           numericInput("obs", "Number of observations to view:", min = 0, max = nrow(mtcars), value = 5)
     ),                     
     conditionalPanel(condition="input.conditionedPanels == 'Plot'",  
+      	   p("The second tab (Plot) is meant to display linear regression (OLS) model fits",
+	       "for the output variable mpg (fuel consumption in miles / gallon).",
+	       "Choose an input (independent) variable (such as weight) from the drop down list.",
+	       "The corresponding straight line will be fit and immediately shown in the plot.",
+	       "Also select another input variable (such as number of cylinders) as color.",
+		   "Different values of this color variable will be shown by color coding."),
            selectInput(inputId = "color",
                        label = "select the color variable",
                        choices = c("cyl", "gear", "vs", "carb"),
@@ -19,8 +34,9 @@ shinyUI(pageWithSidebar(
                        label = "Select input variables",
                        choices = c("wt", "qsec", "am", "disp"),
                        selected = "wt")
-    ),                   
-    h5("See pdf documentation and R codes at https://github.com/sandipan/DevelopingDataProducts")
+    ),
+	p("See the pdf documentation and R codes at ",
+	"https://github.com/sandipan/DevelopingDataProducts")    
   ),
 
   mainPanel(
